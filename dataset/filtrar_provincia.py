24f97a2a99547e048ba41b8259cf9775959e10f9
santvz6 <sant.alvz84@gmail.com>
2025-11-18 20:36:07 +0100

    18nov25

--- dataset/filtrar_provincia.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-nombre_provincia = "Alacant/Alicante"
-df = pd.read_csv("accesibility_data/0802_AccesEdificiosPub.csv")
-
-df_alicante = df[df["provincia"] == nombre_provincia]
-df_alicante.to_csv("accesibility_data/alicante.csv", index=False)
