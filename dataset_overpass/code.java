fc397cab795ea2a6440d070bb264d7919a7385e2
santvz6 <sant.alvz84@gmail.com>
2025-12-05 17:28:03 +0100

    comment

--- dataset_overpass/code.java
+++ dataset_overpass/code.java
@@ -1,7 +1,4 @@
-/* 
-   Salida en CSV - Misma lógica que el Código 1
-   (Accesibilidad general + Baños adaptados específicos)
-*/
+/* (Accesibilidad general + Baños adaptados específicos) */
 [out:csv(
     ::id, 
     ::type, 
@@ -12,19 +9,17 @@
     ::lon
   ; true; ",")][timeout:25];
 
-// 1. Definir el área: Alicante
+// Definimos el área: Alicante
 {{geocodeArea:Alicante}}->.searchArea;
 
-// 2. Buscar EXACTAMENTE lo mismo que en el primer código
 (
-  // Buscar cualquier cosa con la etiqueta 'wheelchair'
+  // Buscamos cualquiera con la etiqueta 'wheelchair'
   node["wheelchair"](area.searchArea);
   way["wheelchair"](area.searchArea);
   relation["wheelchair"](area.searchArea);
   
-  // La línea extra que incluí en el primer código para baños específicos
   node["toilets:wheelchair"="yes"](area.searchArea);
 );
 
-// 3. Salida usando 'center' para que te de coordenadas únicas incluso en edificios
+// Usamos 'center' para obtener coordenadas únicas incluso en edificios
 out center;
